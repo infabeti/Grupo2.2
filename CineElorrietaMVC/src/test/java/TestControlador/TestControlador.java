a89297ebb489c728310b30af8e6f3e04a1d85e4b
mariosoler <mario.solerga@elorrieta-errekamari.com>
2020-11-30 18:22:25 +0100

    Comenzar a refactorizar teses

--- CineElorrietaMVC/src/test/java/TestControlador/TestControlador.java
+++ CineElorrietaMVC/src/test/java/TestControlador/TestControlador.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import Controlador.Controlador;
@@ -18,6 +20,7 @@ import Controlador.ControladorPanelLogin;
 import Controlador.ControladorPanelPeliculas;
 import Controlador.ControladorPanelResumen;
 import Modelo.Modelo;
+import Modelo.Pelicula;
 import Vista.PanelGeneros;
 import Vista.Vista;
 
@@ -75,7 +78,7 @@ public class TestControlador {
 		verify(spyControlador, times(1)).controladorProductosMostrarPanelProductos();
 	}*/
 	@Test
-	public void testNavegarPanelPeliculas(int genero) {
+	public void testNavegarPanelPeliculas() {
 		controlador=new Controlador(modeloMock,vistaMock);
 		
 		controlador.navegarPanelPeliculas(2);
@@ -96,5 +99,41 @@ public class TestControlador {
 		
 		
 	}
+	@Test
+	public void getPeliculasTest() {
+		controlador=new Controlador(modeloMock,vistaMock);
+		
+	    
+		Pelicula[] resultadoEsperado=new Pelicula[16];
+		resultadoEsperado[0] = new Pelicula(1, "Handia", 116);
+		resultadoEsperado[1] = new Pelicula(1, "La lista de Schindler", 197);
+		resultadoEsperado[2] = new Pelicula(1, "Cadena Perpetua", 142);
+		resultadoEsperado[3] = new Pelicula(1, "Million Dollar Baby", 133);
+
+		resultadoEsperado[4] = new Pelicula(2, "Scary movie", 90);
+		resultadoEsperado[5] = new Pelicula(2, "El gran Lebowsky", 119);
+		resultadoEsperado[6] = new Pelicula(2, "La vida de Brian", 94);
+		resultadoEsperado[7] = new Pelicula(2, "Aterriza como puedas", 117);
+
+		resultadoEsperado[8] = new Pelicula(3, "Psicosis", 109);
+		resultadoEsperado[9] = new Pelicula(3, "El resplandor", 146);
+		resultadoEsperado[10] = new Pelicula(3, "Dracula", 155);
+		resultadoEsperado[11] = new Pelicula(3, "Cisne negro", 100);
+
+		resultadoEsperado[12] = new Pelicula(4, "2001: Odisea en el espacio", 142);
+		resultadoEsperado[13] = new Pelicula(4, "La novia de Frankenstein", 75);
+		resultadoEsperado[14] = new Pelicula(4, "El planeta de los simios", 115);
+		resultadoEsperado[15] = new Pelicula(4, "Alien, el octavo pasajero", 117);
+	    
+	    controlador.introducirPrimerasPeliculas();
+		Pelicula[] resultado = controlador.getPeliculas();
+	    assertEquals(resultadoEsperado, resultado);
+	}
+	/*
+	@Test
+	public static void setPeliculasTest() {
+		Controlador.peliculas = peliculas;
+	}
+	*/
 
 }
--- CineElorrietaMVC/src/test/java/TestModelo/ModeloTest.java
+++ CineElorrietaMVC/src/test/java/TestModelo/ModeloTest.java
@@ -1,38 +1,66 @@
 package TestModelo;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import Controlador.ControladorPanelPeliculas;
 import Modelo.Modelo;
 import Modelo.Pelicula;
 
 public class ModeloTest {
-
+	private Modelo modelo;
+	private Modelo modeloMock = mock(Modelo.class);
+	private int resultadoEsperado,resultado;
+	
 	@Test
 	public void testModelo() {
-		Modelo nuevoModelo=new Modelo();
+		modelo=new Modelo();
 	}
+	 @Rule
+	    public TemporaryFolder folder = new TemporaryFolder();
+	 @Test
+	public void escribirErrorEnLogTest() throws IOException {
+		 File file=folder.newFile("errores.txt");
+		 modeloMock.escribirErrorEnLog("Test Error");
+		 
+	 
+		 
+		 
+		 
+	 }
+	 @Test
+		public void esperarTest() {
+			
+			 modeloMock.esperar(3000);
+			 	 
+			 
+		 }
+		 
+	 
+	 
 	
-	/*
 	 @Test
 	public void minutosTotalesTest() {
 		resultadoEsperado=100;
 		
 		
-		controladorPeliculas = new ControladorPanelPeliculas(modeloMock,
-				vistaMock, controladorMock);
+		modelo=new Modelo();
 		//Pelicula peli1=new
 		ArrayList<Pelicula> peliInstancia=new ArrayList();
 		Pelicula peli=new Pelicula(1,"Test Pelicula",100);
 		peliInstancia.add(peli);
-		controladorPeliculas.setPeliculasSabado(peliInstancia);
 		
 		
-		resultado = controladorPeliculas.minutosTotalesSabado();
+		
+		resultado = modelo.minutosTotales(peliInstancia);
 		
 		assertEquals(resultadoEsperado, resultado);
 		
@@ -41,23 +69,49 @@ public class ModeloTest {
 	
 
 }
-	 */
+	 @Test
+		public void resumenEscritoTest() {
+			String resultadoEsperado="";
+			String resultado;
+			resultadoEsperado=resultadoEsperado+"Sábado: \n";
+			resultadoEsperado=resultadoEsperado+"\nDomingo: \n";
+			
+			
+			modelo=new Modelo();
+			
+			
+			
+			resultado = modelo.resumenEscrito();
+			
+			assertEquals(resultadoEsperado, resultado);
+			
+			
+	}
+	 
 	
-	/*
+	
 	 	@Test
-			public void testGetPeliculasSabado() {
-				controladorPeliculas = new ControladorPanelPeliculas(modeloMock,
-						vistaMock, controladorMock);
+			public void getPeliculasSabadoTest() {
+				modelo=new Modelo();
 				
-				ArrayList<Pelicula> peliInstancia=new ArrayList();
-				controladorPeliculas.setPeliculasSabado(peliInstancia);
 			    
 				ArrayList<Pelicula> resultadoEsperado=new ArrayList();
 			    
 			    
-				ArrayList<Pelicula> resultado = controladorPeliculas.getPeliculasSabado();
+				ArrayList<Pelicula> resultado = modelo.getPeliculasSabado();
 			    assertEquals(resultadoEsperado, resultado);
 			}
-	 */
+	 	@Test
+		public void getPeliculasDomingoTest() {
+			modelo=new Modelo();
+			
+		    
+			ArrayList<Pelicula> resultadoEsperado=new ArrayList();
+		    
+		    
+			ArrayList<Pelicula> resultado = modelo.getPeliculasDomingo();
+		    assertEquals(resultadoEsperado, resultado);
+		}
+	 
 
 }

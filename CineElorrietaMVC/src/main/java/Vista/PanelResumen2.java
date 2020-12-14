738df3a6508978c972ba0f0a072930f51022a68e
aiidasuarez <aida.suarezgu@elorrieta-errekamari.com>
2020-12-14 19:25:28 +0100

    vista resumen dos hecha

---
+++ CineElorrietaMVC/src/main/java/Vista/PanelResumen2.java
@@ -0,0 +1,50 @@
+package Vista;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+import Controlador.ControladorPanelLogin;
+import Controlador.ControladorPanelResumen;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+
+public class PanelResumen2 extends JPanel {
+	private static JTextArea txtResumen2;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelResumen2(ControladorPanelResumen controladorPanelResumen) {
+		setLayout(null);
+		
+		JLabel lblResumen2 = new JLabel("RESUMEN:");
+		lblResumen2.setBounds(75, 29, 97, 25);
+		lblResumen2.setFont(new Font("Impact", Font.PLAIN, 20));
+		add(lblResumen2);
+		
+		txtResumen2 = new JTextArea();
+		txtResumen2.setBounds(75, 63, 297, 192);
+		txtResumen2.setEnabled(false);
+		txtResumen2.setDisabledTextColor(Color.BLACK);
+		add(txtResumen2);
+
+		
+		JButton botoncancelar = new JButton("Cancelar");
+		botoncancelar.setBounds(36, 278, 102, 31);
+		add(botoncancelar);
+		
+		JButton botonaceptar = new JButton("Aceptar");
+		botonaceptar.setBounds(313, 278, 102, 31);
+		add(botonaceptar);
+
+	}
+
+	public static JTextArea getTxtResumen() {
+		return txtResumen2;
+	}
+}
